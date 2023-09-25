0274b94af41efd09a755d85e319d909d4c91c9a0
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-25 19:14:55 -0400

    add event listener to each cell
    Upon first click, the cell will be registered as an 'X' for now denoting
    that the cell is occupied and the game board will reflect the spot being
    occupied.

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ const boardCells = document.querySelector('.board-wrapper').children;
 const cellArr = Array.from(boardCells);
 
 const gameBoard = (() => {
-  const board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-  ];
+  const board = [false, false, false, 
+                false, false, false,
+                false, false, false];
+  
   for (let i = 0; i < cellArr.length; i++) {
-    cellArr[i].textContent = 'X';
+    cellArr[i].addEventListener('click', event => {
+      if (board[i] == false) {
+        cellArr[i].textContent = 'X';
+        board[i] = true;
+      } else {
+        console.log('spot occupied');
+      }
+    board.forEach(x => console.log(x));
+    });
   }
 })();
+
+
 
 const personFactory = (name) => {
   const play = () => {
